9cab5ce178e9693e50d0538614a92bd3f4ae095f
Veasna <somveasna00@gmail.com>
2026-03-25 21:04:24 +0700

    add spring monthly exam

---
+++ src/main/java/com/sna/spring_monthly_exam/SpringMonthlyExamApplication.java
@@ -0,0 +1,13 @@
+package com.sna.spring_monthly_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMonthlyExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMonthlyExamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sna/spring_monthly_exam/SpringMonthlyExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sna.spring_monthly_exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMonthlyExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
